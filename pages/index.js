b214eddfe915d999841b5832485f585bff00d771
vai91 <mehmetefeekiner@gmail.com>
2019-09-24 20:41:02 +0200

    Add Prices class based component, render prices conditionally

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
+import Fetch from 'isomorphic-unfetch';
 import Layout from '../components/layout';
+import Prices from '../components/prices';
 
-const Index = () => (
+const Index = (props) => (
 <Layout>
     <div>
         <h1>Welcome to BitzPrice</h1>
+        <p>Check current Bitcoin rate</p>
+        <Prices bpi={props.bpi} />
     </div>
 </Layout>
 );
+
+//required lifecycle method
+Index.getInitialProps = async function(){
+    const res = await Fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json();
+
+    return {
+        bpi: data.bpi
+    }
+}
 export default Index;
